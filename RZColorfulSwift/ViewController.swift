beaed21f897d19a57950d35c4a845599dad1a67f
rztime <rztime@vip.qq.com>
2018-07-09 10:34:10 +0800

    Initial Commit

---
+++ RZColorfulSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RZColorfulSwift
+//
+//  Created by 若醉 on 2018/7/9.
+//  Copyright © 2018年 rztime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
